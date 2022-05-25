74060f447c8bc1c63991986f599e2e45775adf0e
FagnerAureliano <fagner2aurelian@gmail.com>
2022-05-25 15:15:35 -0300

    created auth with token validation

--- src/use-cases/routes.ts
+++ src/use-cases/routes.ts
@@ -1,6 +1,7 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import { CreateUserController } from "./create-user/create-user-controller";
-import { AuthenticateUserController } from './authenticate-user/authenticate-user-controller';
+import { AuthenticateUserController } from "./authenticate-user/authenticate-user-controller";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 export const router = Router();
 
@@ -9,4 +10,13 @@ const authenticateUserController = new AuthenticateUserController();
 
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
- 
+
+router.get("/courses", ensureAuthenticated,(req, res) => {
+ return res.json([
+    { id: 1, name: "ReactJS" },
+    { id: 2, name: "React Native" },
+    { id: 3, name: "Angular" },
+    { id: 4, name: "NodeJS" },
+    { id: 5, name: "Elixir" },
+  ]);
+});
